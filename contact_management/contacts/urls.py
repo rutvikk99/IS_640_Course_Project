0918c094f77c33269b5ef308ac028cc809d1ddd0
rutvikk99 <rutvikk99@gmail.com>
2023-11-27 13:19:49 -0800

    Basic functions working

--- contact_management/contacts/urls.py
+++ contact_management/contacts/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('new/', views.new_contact, name='new_contact'),
+    path('contact/<int:contact_id>/', views.contact_details, name='contact_details'),
+    path('contact/<int:contact_id>/edit/', views.edit_contact, name='edit_contact'),
+    path('contact/<int:contact_id>/delete/', views.delete_contact, name='delete_contact'),
 ]
--- contact_management/contacts/views.py
+++ contact_management/contacts/views.py
@@ -1,5 +1,5 @@
 # contacts/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
 from .forms import ContactForm
 
@@ -17,3 +17,29 @@ def new_contact(request):
         form = ContactForm()
     
     return render(request, 'contacts/new_contact.html', {'form': form})
+
+def contact_details(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    return render(request, 'contacts/contact_details.html', {'contact': contact})
+
+def edit_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_details', contact_id=contact.id)
+    else:
+        form = ContactForm(instance=contact)
+
+    return render(request, 'contacts/edit_contact.html', {'form': form, 'contact': contact})
+
+def delete_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('home')
+
+    return render(request, 'contacts/delete_contact.html', {'contact': contact})
